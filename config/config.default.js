919d265dc985af97e80831c3b43f7c2225aecc47
南峰 <zengnq@yunjiglobal.com>
2021-10-22 16:44:28 +0800

    全局异常处理

--- config/config.default.js
+++ config/config.default.js
@@ -15,10 +15,23 @@ module.exports = appInfo => {
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1634199877026_3436';
 
-  // add your middleware config here
-  config.middleware = [];
-
-  // add your user config here
+  config.middleware = [ 'errorHandler' ];
+  config.errorHandler = {
+    // 通用配置（以下是重点）
+    // enable:true, // 控制中间件是否开启。
+    // match: '/user/list', // 设置只有符合某些规则的请求才会经过这个中间件（匹配路由）
+    // ignore:'/shop' // 设置符合某些规则的请求不经过这个中间件。
+    /**
+        注意：
+        1. match 和 ignore 不允许同时配置
+        **/
+    // match 和 ignore 支持多种类型的配置方式：字符串、正则、函数（推荐）
+    // match(ctx) {
+    //     // 只有 ios 设备才开启
+    //     const reg = /iphone|ipad|ipod/i;
+    //     return reg.test(ctx.get('user-agent'));
+    // },
+  };
   const userConfig = {
     // myAppName: 'egg',
   };
